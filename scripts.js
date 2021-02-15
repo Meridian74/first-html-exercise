5060b9c43fb042c5aae69a045f32ab52c2aef191
meridian74 <meridian@freemail.hu>
2021-02-15 23:50:53 +0100

    second task is done

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
 $("#fox-1").text("Macrotis");
 $("#fox-2").css("border-color", "purple");
 $("p.first-task").css("background", "yellow");
+
+let color = "purple";
+let number = 10;
+let word = "cool";
+
+if (color === "purple") {
+   $("p.north").css("background", "purple");
+}
+
+if (number > 100) {
+   $("p.east").css("font-size", "16px");
+   $("p.east").text("WoW, de nagy szám");
+}
+else {
+   $("p.east").css("font-size", "12px");
+   $("p.east").text("Ez csak egy közönséges szám");
+}
+
+if (word === "cool") {
+   $("p.south").text("A DOM Ereje");
+}
+else {
+   $("p.west").text("A DOM Ereje");
+}
+
+
